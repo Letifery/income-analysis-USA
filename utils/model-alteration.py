2d4714012abd3ecb25b0c96ea7f2ae9f4316370d
Letifery <34723172+Letifery@users.noreply.github.com>
2021-12-11 00:29:46 +0100

    Changed plotting and added several functions
    - Heavily enhanced the plotting function to plot multiple accuracy vectory and automatically label those
    - Added hyperparameter optimizations for the kNN and perceptron classifiers

--- utils/model-alteration.py
+++ utils/model-alteration.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import KNeighborsClassifier
 
 class ModelAlteration():
     def strat_kfold_evaluation(
@@ -42,9 +45,129 @@ class ModelAlteration():
             if accuracy[i] >= max(accuracy[:-1]): best_fold = index
         return(accuracy, best_fold)
 
-    def plot_accuracy(self, acc:[[float]]):
-        plt.xlabel('fold')
+    def plot_accuracy(self, acc:[[float]], xlab:str, legend:[str], xaxis:[]=[]):
+        print(legend, xaxis)
+        plt.xlabel(xlab)
         plt.ylabel('Accuracy [%]')
         acc = acc if len(acc)>0 else [acc]
-        for accuracy in acc:
-            plt.plot(range(len(accuracy)), accuracy)
+        if not xaxis:
+            for i, accuracy in enumerate(acc):
+                plt.plot(range(len(accuracy)), accuracy, label = legend[i])
+        else:
+            for i, accuracy in enumerate(acc):
+                plt.plot(xaxis, accuracy, label = legend[i])  
+        plt.legend(loc="upper left")
+
+    def optimize_knn(self, 
+        df, 
+        target:int,
+        neighbours:[int] = list(range(1,11)), 
+        metric:[int]=[1,2,3],
+        folds:int = 10,
+        plot:bool=True):
+        '''
+        Attempts to find the most optimal model parameters for the k-nearest-
+        neighbours (kNN) classifier by finding the best fold for each permutation 
+        of the parameters. The best fold is determined by strat_kfold_evaluation(). 
+        The accuracy of all best folds is then compared and the  parameters of 
+        the best fold are returned (in addition to the fold itself)
+
+        Parameters
+        ------------
+        df : dataframe
+            Your datatable
+        target : int 
+            The index of your target column
+        neighbours : [int]
+            A list which contains the number of neighbors which should be used in kNN.
+        metric : [int] 
+            Which metric should be used for kNN
+            1 - Manhattan
+            2 - Euclidean
+            3>= - Minkowski
+        folds : int 
+            How often your dataframe should be split in strat_kfold_evaluation
+        plot : bool
+            Plots the accuracies over each fold
+
+        Returns
+        ------------
+        best_fold: (np.array(int), np.array(int))
+            An indexlist of the fold which has performed best overall
+        k : int
+            The number of neighbours where the model has performed best 
+        used_metric : int 
+            The metric used where the model performed best
+        '''
+        best_acc, best_model, fold_acc = 0, 0, [[None for _ in neighbours] for _ in metric]
+        epoch, end = 1, len(neighbours)*len(metric)
+        for i,m in enumerate(metric):
+            for j,k in enumerate(neighbours):
+                model = KNeighborsClassifier(n_neighbors=k, p = m)
+                fold_acc[i][j], tmp_fold = (lambda x: [max(x[0]), x[1]])(self.strat_kfold_evaluation(df, model, target, folds))
+                if fold_acc[i][j] > best_acc: 
+                    best_acc = fold_acc[i][j]
+                    best_model = (tmp_fold, k, m)
+                print("Epoch %s/%s | neighbours=%s, metric=%s, Accuracy=%s" % (epoch, end, k, m, fold_acc[i][j]))
+                epoch += 1
+        if plot: self.plot_accuracy(fold_acc, "Number of neighbours", list(map(lambda x: "Metric " + x, list(map(str, metric)))), neighbours)
+        return(best_model)
+
+    def optimize_perceptron(self, 
+        df, 
+        target:int,
+        learning_rate:[float] = np.linspace(1, 20, num=20), 
+        penalty:[int]=[0,1,2,3],
+        folds:int = 10,
+        plot:bool=True):
+        '''
+        Attempts to find the most optimal model parameters for the perceptron 
+        classifier by finding the best fold for each permutation of the 
+        parameters. The best fold is determined by strat_kfold_evaluation(). 
+        The accuracy of all best folds is then compared and the parameters of 
+        the best fold are returned (in addition to the fold itself)
+
+        Parameters
+        ------------
+        df : dataframe
+            Your datatable
+        target : int 
+            The index of your target column
+        learning_rate : [float]
+            A list containing the number of learning_rates the algorithm should
+            try out
+        penalty : [int] 
+            Which penalty should be used
+            0 - None
+            1 - l1
+            2 - l2
+            3 - elasticnet
+        folds : int 
+            How often your dataframe should be split in strat_kfold_evaluation
+        plot : bool
+            Plots the accuracies over each fold
+
+        Returns
+        ------------
+        best_fold: (np.array(int), np.array(int))
+            An indexlist of the fold which has performed best overall
+        l : float
+            The best learning rate for this dataset+penalty 
+        r : int 
+            Best penalty   
+        '''
+        best_acc, best_model, fold_acc = 0, 0, [[None for _ in learning_rate] for _ in penalty]
+        epoch, end = 1, len(learning_rate)*len(penalty)
+        penalty = list(map((lambda x, d={0:None, 1:"l1", 2:"l2", 3:"elasticnet"}: d[x]), penalty)) 
+
+        for i, m in enumerate(penalty):
+            for j, k in enumerate(learning_rate):
+                model = Perceptron(eta0=k, penalty=m)
+                fold_acc[i][j], tmp_fold = (lambda x: [max(x[0]), x[1]])(self.strat_kfold_evaluation(df, model, target, folds))
+                if fold_acc[i][j] > best_acc: 
+                    best_acc = fold_acc[i][j]
+                    best_model = (tmp_fold, k, m)
+                print("Epoch %s/%s | learning_rate=%s, penalty=%s, Accuracy=%s" % (epoch, end, k, m, fold_acc[i][j]))
+                epoch += 1
+        if plot: self.plot_accuracy(fold_acc, "Used learning_rate", list(map(lambda x: "penalty: " + str(x), penalty)), list(learning_rate))
+        return(best_model)
